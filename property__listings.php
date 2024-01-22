0dead255dc005fd599dba3724a167c729166b848
bckyrd <95740688+bckyrd-io@users.noreply.github.com>
2024-01-22 08:06:07 -0800

    categories filter fixed

--- property__listings.php
+++ property__listings.php
@@ -211,11 +211,11 @@ $uniquePriceRanges = array_keys($uniquePriceRanges);
 
     <!-- Scripts -->
     <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-    <script src="assets/libs/isotope.pkgd.min.js"></script>
+    <script src="./assets/libs/isotope.pkgd.min.js"></script>
     <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/sidebarmenu.js"></script>
     <script src="assets/js/app.min.js"></script>
-   
+
 
     <script>
         $(document).ready(function() {
